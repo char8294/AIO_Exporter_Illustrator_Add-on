7217e483d15d21278227f8d9b8d48f5d0c972348
char8294 <char8294@outlook.com>
2026-07-13 19:01:09 +0700

    1.5.2

--- scripts/AIO_Exporter.jsx
+++ scripts/AIO_Exporter.jsx
@@ -6,7 +6,7 @@
 */
 (function () {
     var APP_NAME = "AIO Exporter";
-    var APP_VERSION = "1.5.1";
+    var APP_VERSION = "1.5.2";
     var DEFAULT_BASE_NAME = "AIO_Exporter";
 
     function trim(value) {
